ecc6755f57d4033e528cee78947faa6ca74705b5
marlon <md-vasquezs@corhuila.edu.co>
2024-02-23 14:35:04 -0500

    se  mejora el modelo de la api

--- products/serializers.py
+++ products/serializers.py
@@ -5,7 +5,7 @@ from .models import  Category,Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'name','category','description','price')
+        fields = ('id', 'name','image','category','description','price','price_type')
         
 class categorySerializer(serializers.ModelSerializer):
     class Meta:
